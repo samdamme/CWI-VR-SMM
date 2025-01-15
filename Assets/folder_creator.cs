903c269075389a0b7fc44b9be886f9871182135d
Sam Van Damme (UGent-imec) <sam.vandamme@ugent.be>
2025-01-15 16:04:12 +0100

    Small fixes

---
+++ Assets/folder_creator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class folder_creator : MonoBehaviour
+{
+    private void Awake()
+    {
+        string pt_folder = "./CWI-VR-SMM_Data/position_tracking/";
+        FileInfo file = new FileInfo(pt_folder);
+        file.Directory.Create();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
